2175f883685fee1e9ef2006cafdb8913f8ebebe5
Violet Peña <violetpena@gmail.com>
2017-12-02 12:20:54 -0800

    Deleted Header; customized interface.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 const stories = require.context('../src/components/', true, /\/stories\.js$/);
 
 function loadStories() {
   return stories.keys().forEach(stories);
 };
+
+setOptions({
+  name: '1337 Storybook',
+  url: 'https://alert-oriole.glitch.me',
+  downPanelInRight: true,
+});
 
 configure(loadStories, module);
