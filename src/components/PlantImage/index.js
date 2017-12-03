4230cf46fdd67ad9ee92a2faf3fbe0b8c102bf26
Violet Peña <violetpena@gmail.com>
2017-12-03 15:05:24 -0800

    Remove log.

--- src/components/PlantImage/index.js
+++ src/components/PlantImage/index.js
@@ -4,7 +4,6 @@ import './styles.css';
 
 export class PlantImage extends React.Component {
   render() {
-    console.log(this.props.imageUrl);
     return (
       <div className="plant-image" style={ this.props.imageUrl && { 'backgroundImage': `url(data:image/png;base64,${ localStorage.getItem(this.props.imageUrl) })` }} />
     );
