13dc17b1b1f388a918b4adbf7f4be5078d21488b
Violet Peña <violetpena@gmail.com>
2017-12-02 18:02:32 -0800

    Buttons.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import './styles.css';
 
+export const BUTTON_TYPES = {
+  DEFAULT: 'button',
+  POSITIVE: 'button button-save',
+  DANGER: 'button button-remove',
+  FUNCTION: 'button button-download',
+}
+
 export class Button extends React.Component {
   render() {
     return (
-      <button className="button">
+      <button
+        className={ this.props.type || BUTTON_TYPES.DEFAULT }
+      >
         { this.props.content }
       </button>
     );
--- src/components/Button/stories.js
+++ src/components/Button/stories.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Button } from './';
+import { Button, BUTTON_TYPES } from './';
 
 const stories = storiesOf('Button', {});
 
@@ -9,6 +9,33 @@ stories.add('default', () => {
   return (
     <Button
       content={ "Button" }
+    />
+  );
+});
+
+stories.add('positive', () => {
+  return (
+    <Button
+      type={ BUTTON_TYPES.POSITIVE }
+      content={ "Save Plant" }
+    />
+  );
+});
+
+stories.add('danger', () => {
+  return (
+    <Button
+      type={ BUTTON_TYPES.DANGER }
+      content={ "Remove Plant" }
+    />
+  );
+});
+
+stories.add('function', () => {
+  return (
+    <Button
+      type={ BUTTON_TYPES.FUNCTION }
+      content={ "Download Plants List" }
     />
   );
 });
